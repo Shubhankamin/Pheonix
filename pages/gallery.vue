c7ba216d699001622aadf83486da1592a4776326
shubhank <aminshubhank@gmail.com>
2025-02-17 14:49:34 +0530

    gallery images and loader added

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <v-container fluid class="">
+    <div v-if="isLoading" class="loading-spinner">
+      <div class="loading-spinner-inner">
+        <div class="loading-spinner-circle"></div>
+        <div class="loading-spinner-circle"></div>
+        <div class="loading-spinner-circle"></div>
+        <div class="loading-spinner-circle"></div>
+        <div class="loading-spinner-circle"></div>
+      </div>
+    </div>
+    <v-container fluid class="" v-show="!isLoading">
       <v-row class="pa-0">
         <v-col class="pa-0">
           <Nav />
@@ -70,6 +79,13 @@ const show = (src, itemsImage, index) => {
     viewerInstance.value.view(index);
   }
 };
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 1000); // Adjust time as needed
+});
 
 const flatImageList = computed(() =>
   imageRows.flat().map((image) => ({
@@ -86,29 +102,28 @@ const imageRows = [
     { url: "/images/gallery/gal-1.jpeg", flex: 1 },
     { url: "/images/gallery/gal-2.jpeg", flex: 1 },
     { url: "/images/gallery/gal-3.jpeg", flex: 1 },
-  ],
-  [
     {
       url: "/images/gallery/gal-4.jpeg",
       flex: 1,
     },
-    { url: "/images/gallery/gal-5.jpeg", flex: 1 },
   ],
   [
+    {
+      url: "/images/gallery/gal-5.jpeg",
+      flex: 1,
+    },
     { url: "/images/gallery/gal-6.jpeg", flex: 1 },
     { url: "/images/gallery/gal-7.jpeg", flex: 1 },
     { url: "/images/gallery/gal-8.jpeg", flex: 1 },
-    { url: "/images/gallery/gal-9.jpeg", flex: 1 },
   ],
   [
+    { url: "/images/gallery/gal-9.jpeg", flex: 1 },
     { url: "/images/gallery/gal-10.jpeg", flex: 1 },
-    {
-      url: "/images/gallery/gal-11.jpeg",
-      flex: 1,
-    },
+    { url: "/images/gallery/gal-11.jpeg", flex: 1 },
     { url: "/images/gallery/gal-12.jpeg", flex: 1 },
     { url: "/images/gallery/gal-13.jpeg", flex: 1 },
   ],
+  // [{ url: "/images/gallery/gal-13.jpeg", flex: 1 }],
 ];
 </script>
 
@@ -152,5 +167,66 @@ body {
   background-position: center center;
   box-shadow: 0 6px 20px rgba(0, 0, 0, 0.3), 0 6px 20px rgba(0, 0, 0, 0.15),
     0 6px 20px rgba(0, 0, 0, 0.7);
+}
+
+.loading-spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: black; /* Change as needed */
+  z-index: 9999;
+}
+
+.loading-spinner-inner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.loading-spinner-circle {
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background-color: red;
+  margin: 0 5px;
+  animation: loading-spinner 1s ease-in-out infinite;
+}
+
+.loading-spinner-circle:nth-child(2) {
+  animation-delay: 0.2s;
+}
+
+.loading-spinner-circle:nth-child(3) {
+  animation-delay: 0.4s;
+}
+
+.loading-spinner-circle:nth-child(4) {
+  animation-delay: 0.6s;
+}
+
+.loading-spinner-circle:nth-child(5) {
+  animation-delay: 0.8s;
+}
+
+@keyframes loading-spinner {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  20% {
+    transform: scale(1.5);
+    opacity: 0.5;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
 }
 </style>

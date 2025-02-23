c9c60a5ba6446be560f774eb24368831885be14f
shubhank <aminshubhank@gmail.com>
2025-02-23 08:52:12 +0530

    our program image fixed

--- components/OurPrograms.vue
+++ components/OurPrograms.vue
@@ -26,7 +26,7 @@
         loop="true"
         :space-between="10"
         :autoplay="{ delay: 3000, disableOnInteraction: false }"
-        class="mySwiper mb-5"
+        class="mySwiper mb-5 d-none d-md-block"
       >
         <swiper-slide v-for="(item, i) in sliderImages" :key="i">
           <v-img
@@ -40,6 +40,51 @@
           >
             <v-card
               style="position: absolute; bottom: 5%; right: 5%; left: 5%"
+              min-width="300"
+              class="px-4 py-2 image-card"
+              elevation="0"
+            >
+              <div class="d-flex justify-space-between">
+                <p style="text-transform: uppercase" class="ubuntu-regular-h3">
+                  {{ item.title }}
+                </p>
+                <v-icon color="#ffb400">mdi-arrow-top-right-thin</v-icon>
+              </div>
+              <p class="ubuntu-regular-h4">{{ item.description }}</p>
+              <p class="ubuntu-regular-h4">{{ item.description2 }}</p>
+            </v-card>
+          </v-img>
+        </swiper-slide>
+      </swiper>
+      <swiper
+        :modules="swiperModules"
+        :pagination="{ clickable: true }"
+        :loop="true"
+        :slides-per-view="3"
+        :breakpoints="{
+          2560: { slidesPerView: 3 },
+          1440: { slidesPerView: 3 },
+          1024: { slidesPerView: 3 },
+          767: { slidesPerView: 2 },
+          320: { slidesPerView: 1 },
+        }"
+        loop="true"
+        :space-between="10"
+        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+        class="mySwiper mb-5 d-block d-md-none"
+      >
+        <swiper-slide v-for="(item, i) in sliderImagesMob" :key="i">
+          <v-img
+            class="bg-image"
+            height="400"
+            width="100%"
+            style="position: relative; object-position: top"
+            cover
+            :src="item.src"
+            :lazy-src="item.src"
+          >
+            <v-card
+              style="position: absolute; bottom: 5%"
               min-width="300"
               class="px-4 py-2 image-card"
               elevation="0"
@@ -71,6 +116,28 @@ const swiperModules = [Autoplay];
 const sliderImages = [
   {
     src: "/images/slider-1.png",
+    title: "Monthly 8 Class",
+    description:
+      "Stay consistent and dedicated in your martial arts journey with 8 expertly designed karate classes offered every month.",
+  },
+
+  {
+    src: "/images/prog-2.png",
+    title: "Saturday 10-11",
+    description:
+      "Kickstart your weekend with karate training every Saturday from 10 to 11 AM.",
+  },
+
+  {
+    src: "/images/prog-1.jpg",
+    title: "Sunday 10-11",
+    description:
+      "Wrap up your weekend with a dynamic karate session every Sunday, 10 to 11 AM.",
+  },
+];
+const sliderImagesMob = [
+  {
+    src: "/images/prog-5.jpeg",
     title: "Monthly 8 Class",
     description:
       "Stay consistent and dedicated in your martial arts journey with 8 expertly designed karate classes offered every month.",
